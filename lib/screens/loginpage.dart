e065bb2a0ba376fcc43f966028ab8ee44f4f2ff0
bgsprayogi <prayogigres@gmail.com>
2025-05-14 17:06:36 +0700

    feat: slicing login page and navbar bottom

--- lib/screens/loginpage.dart
+++ lib/screens/loginpage.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kopiqu/widgets/customtextfield.dart';
 
-
 class LoginPage extends StatelessWidget {
   LoginPage({super.key});
 
@@ -11,63 +10,57 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFEFEFEF),
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
       body: Padding(
         padding: const EdgeInsets.all(24),
-        child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Image.asset(
-                  'assets/kopiqu.png',
-                  width: 150,
-                ),
-                const SizedBox(height: 24),
-                Image.asset(
-                  'assets/iconlogin.png',
-                  height: 150,
-                ),
-                const SizedBox(height: 24),
-                CustomTextField(
-                  label: 'Email',
-                  hintText: 'Masukkan Email',
-                  controller: emailController,
-                ),
-                const SizedBox(height: 16),
-                CustomTextField(
-                  label: 'Password',
-                  hintText: 'Masukkan Password',
-                  obscureText: true,
-                  controller: passwordController,
-                ),
-                const SizedBox(height: 24),
-                CustomButton(
-                  text: 'Masuk',
-                  onPressed: () {
-                    // action login di sini
-                  },
-                ),
-                const SizedBox(height: 16),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text('Belum memiliki akun? '),
-                    GestureDetector(
-                      onTap: () {
-                        // navigate ke halaman registrasi
-                      },
-                      child: const Text(
-                        'Registrasi',
-                        style: TextStyle(
-                          color: Colors.blue,
-                          fontWeight: FontWeight.bold,
-                        ),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 35), // jarak dari atas ke logo
+              Image.asset('assets/kopiqu.png', width: 170),
+              const SizedBox(height: 32), // jarak antar logo ke icon login
+              Center(child: Image.asset('assets/iconlogin.png', height: 220)),
+              const SizedBox(height: 32), // jarak ke textfield
+              CustomTextField(
+                label: 'Email',
+                hintText: 'Masukkan Email',
+                controller: emailController,
+              ),
+              const SizedBox(height: 20),
+              CustomTextField(
+                label: 'Password',
+                hintText: 'Masukkan Password',
+                obscureText: true,
+                controller: passwordController,
+              ),
+              const SizedBox(height: 70), // jarak textfield ke tombol
+              CustomButton(
+                text: 'Masuk',  
+                onPressed: () {
+                  // action login di sini
+                },
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Belum memiliki akun? '),
+                  GestureDetector(
+                    onTap: () {
+                      // navigate ke halaman registrasi
+                    },
+                    child: const Text(
+                      'Registrasi',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                  ],
-                ),
-              ],
-            ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
