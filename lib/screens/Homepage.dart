1d2147a50f9c79cff300b418ec722f725734706e
nafisaa11 <jnafisah50@gmail.com>
2025-05-15 12:50:29 +0700

    mau pull

--- lib/screens/Homepage.dart
+++ lib/screens/Homepage.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:kopiqu/controllers/banner_controller.dart';
 import 'package:kopiqu/models/kopi.dart';
 import 'package:kopiqu/widgets/KopiQu_header.dart';
-import 'package:kopiqu/widgets/kopi_card.dart';
-import 'package:kopiqu/widgets/search_widget.dart';
-import 'package:kopiqu/widgets/tag_list.dart';
+import 'package:kopiqu/widgets/Homepage/kopi_card.dart';
+import 'package:kopiqu/widgets/Homepage/search_widget.dart';
+import 'package:kopiqu/widgets/Homepage/tag_list.dart';
 import 'package:kopiqu/widgets/navbar_bottom.dart';
-import 'package:kopiqu/widgets/banner_slider.dart'; // Ganti dari banner_carousel.dart
+import 'package:kopiqu/widgets/Homepage/banner_slider.dart'; 
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -27,7 +27,7 @@ void initState() {
   // Memulai auto scroll untuk banner
   bannerController.startAutoScroll(bannerImages, () {
     if (mounted) {
-      setState(() {}); // Memperbarui tampilan setelah animasi selesai
+      setState(() {}); 
     }
   });
 }
@@ -46,67 +46,59 @@ void initState() {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: null,
-      body: Stack(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 100),
-            child: CustomScrollView(
-              slivers: [
-                //search 
-                SliverToBoxAdapter(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: SearchWidget(
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: null,
+    body: Column(
+      children: [
+        const KopiQuHeader(), // Tetap di atas
+         const SizedBox(height: 16),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: SearchWidget(), // Juga tetap di atas
+        ),
+        const SizedBox(height: 16),
+
+        // Scrollable content
+        Expanded(
+          child: CustomScrollView(
+            slivers: [
+              SliverToBoxAdapter(
+                child: BannerSlider(
+                  bannerImages: bannerImages,
+                  pageController: bannerController.pageController,
+                ),
+              ),
+              const SliverToBoxAdapter(child: SizedBox(height: 20)),
+              const SliverToBoxAdapter(child: TagList()),
+              const SliverToBoxAdapter(child: SizedBox(height: 16)),
+              SliverPadding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                sliver: SliverGrid(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 16,
+                    mainAxisSpacing: 16,
+                    childAspectRatio: 0.75,
+                  ),
+                  delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                      return CoffeeCard(kopi: kopiList[index]);
+                    },
+                    childCount: kopiList.length,
                   ),
                 ),
               ),
-              const SliverToBoxAdapter(child: SizedBox(height: 16)),
-
-                //banner
-                SliverToBoxAdapter(
-                  child: BannerSlider( // Ganti dari BannerCarousel
-                    bannerImages: bannerImages,
-                    pageController: bannerController.pageController,
-                  ),
-                ),
-                const SliverToBoxAdapter(child: SizedBox(height: 20)),
-                const SliverToBoxAdapter(child: TagList()),
-                const SliverToBoxAdapter(child: SizedBox(height: 16)),
-                SliverPadding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  sliver: SliverGrid(
-                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      crossAxisSpacing: 16,
-                      mainAxisSpacing: 16,
-                      childAspectRatio: 0.75,
-                    ),
-                    delegate: SliverChildBuilderDelegate(
-                      (context, index) {
-                        return CoffeeCard(kopi: kopiList[index]);
-                      },
-                      childCount: kopiList.length,
-                    ),
-                  ),
-                ),
-              ],
-            ),
+              const SliverToBoxAdapter(child: SizedBox(height: 50)),
+            ],
           ),
-          const Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            child: KopiQuHeader(),
-          ),
-        ],
-      ),
-      bottomNavigationBar: NavbarBottom(
-        selectedIndex: selectedIndex,
-        onItemSelected: onItemSelected,
-      ),
-    );
-  }
+        ),
+      ],
+    ),
+    bottomNavigationBar: NavbarBottom(
+      selectedIndex: selectedIndex,
+      onItemSelected: onItemSelected,
+    ),
+  );
+}
 }
